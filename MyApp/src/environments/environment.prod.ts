18abd79a6bbe615f843b635ec01d7735e47825b2
anthonyging12 <65613944+anthonyging12@users.noreply.github.com>
2025-05-16 15:46:23 -0400

    Remove OpenAI API key from code

--- MyApp/src/environments/environment.prod.ts
+++ MyApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'mymoviesapp1234-b8dhf8b8hdbpfzgk.eastus2-01.azurewebsites.net',
+  apiUrl: 'https://mymoviesappnew123-a5cugse7g8esg2gu.eastus2-01.azurewebsites.net',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
