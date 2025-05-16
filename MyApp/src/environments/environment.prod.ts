9733cc3f4ef1aa60fb07137ea07e8676a495bfef
anthonyging12 <65613944+anthonyging12@users.noreply.github.com>
2025-05-16 14:50:35 -0400

    Initial commit with .NET + Angular2

--- MyApp/src/environments/environment.prod.ts
+++ MyApp/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'mymoviesapp1747331272.azurewebsites.net',
+  apiUrl: 'mymoviesapp1234-b8dhf8b8hdbpfzgk.eastus2-01.azurewebsites.net',
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
--- MyApp/src/environments/environment.ts
+++ MyApp/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: '/api',                           // ← proxy from 4200 → 5091
+  apiUrl: 'http://localhost:5091',                           // ← proxy from 4200 → 5091
   tmdbImageBaseUrl: 'https://image.tmdb.org/t/p/w500'
 };
